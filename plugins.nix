974844b47aa3799b82075cff1b98e2f95e367bf8
vebly <>
2025-03-27 09:09:40 +0100

    Added continuum

--- plugins.nix
+++ plugins.nix
@@ -1,6 +1,12 @@
-{tmuxPlugins}: with tmuxPlugins; [
-  { plugin = resurrect; }
+{tmuxPlugins}:
+with tmuxPlugins; [
+  {plugin = resurrect;}
+  {
+    plugin = continuum;
+    config = ''
+      set -g @continuum-restore 'on'
+    '';
+  }
   # { plugin = open; }
   # { plugin = copycat; }
 ]
-
