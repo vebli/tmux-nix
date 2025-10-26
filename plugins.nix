64c55c77f5b212848a2bee4b95ce257dabaca158
vebly <>
2025-10-26 22:06:22 +0100

    removed resurrect

--- plugins.nix
+++ plugins.nix
@@ -1,12 +1,12 @@
 {tmuxPlugins}:
 with tmuxPlugins; [
-  {plugin = resurrect;}
-  {
-    plugin = continuum;
-    config = ''
-      set -g @continuum-restore 'on'
-    '';
-  }
+  # {plugin = resurrect;}
+  # {
+  #   plugin = continuum;
+  #   config = ''
+  #     set -g @continuum-restore 'on'
+  #   '';
+  # }
   { plugin = vim-tmux-navigator; }
   # { plugin = copycat; }
 ]
