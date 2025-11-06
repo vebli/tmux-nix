4d9aa40452ffd9650460dca84b7f150dddd69585
vebly <>
2025-11-06 19:40:33 +0100

    open_repo.sh

---
+++ scripts/open-repo.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+url=$(git remote get-url origin) 
+
+if [[ $url == *github.com* ]]; then
+    if [[ $url == git@* ]]; then
+        url="${url#git@}"
+        url="${url/:/\/}" 
+        url="https://$url"
+    fi
+    xdg-open "$url"
+else
+    echo "This repository is not hosted on GitHub"
+fi
